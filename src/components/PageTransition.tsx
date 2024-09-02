6b365010a0ac1ef963977cda8c8d4ac4f8ebc375
Vijay Kumar Das <vijaydas0078@gmail.com>
2024-09-02 12:30:13 +0530

    page switch animation add

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,12 +1,31 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 
 const PageTransition = ({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) => {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  const pathname = usePathname();
   return (
-    <div>PageTransition</div>
+    <AnimatePresence>
+      <div key={pathname}>
+        <motion.div
+          initial = {{
+            opacity:1
+          }}
+          animate ={{
+            opacity:0,
+            transition:{delay:1, duration:0.4, ease: "easeInOut"},
+          }}
+          className="h-screen w-screen fixed bg-black top-0 pointer-events-none"
+        />
+          {children}
+      </div>
+    </AnimatePresence>
   )
 }
 
